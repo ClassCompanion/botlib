fc4875f72aa06e05e66c11ef32846673b3926bb2
LovroG05 <lovro.govekar@gmail.com>
2022-02-06 18:47:39 +0100

    added serverlib

--- src/main/kotlin/org/classcompanion/botlib/ServerLib.kt
+++ src/main/kotlin/org/classcompanion/botlib/ServerLib.kt
@@ -1,15 +1,15 @@
 package org.classcompanion.botlib
 
-import com.rabbitmq.client.*
+import com.rabbitmq.client.Channel
+import com.rabbitmq.client.Connection
+import com.rabbitmq.client.ConnectionFactory
 import org.classcompanion.botlib.consumes.DefaultConsume
-import org.classcompanion.botlib.rabbitmq.OnConsume
 import org.classcompanion.botlib.rabbitmq.RabbitmqManager
 
-
-class BotLib(baseUrl: String, guildId: String) {
+class ServerLib(baseUrl: String) {
 	private val serverQueueName = "server"
 	private val botQueueName = "bot"
-	private var factory: ConnectionFactory = RabbitmqManager.createFactory("test", "tset", "/", baseUrl, 5672)
+	var factory: ConnectionFactory = RabbitmqManager.createFactory("test", "tset", "/", baseUrl, 5672)
 	private var connection: Connection? = RabbitmqManager.makeConnection(factory)
 	private var channel: Channel? = connection?.let { RabbitmqManager.createChannel(it) }
 	init {
@@ -18,12 +18,12 @@ class BotLib(baseUrl: String, guildId: String) {
 	}
 
 	fun sendMessage(msg: String) {
-		RabbitmqManager.basicPublish(channel!!, botQueueName, msg)
+		RabbitmqManager.basicPublish(channel!!, serverQueueName, msg)
 		println("[x] Sent '$msg'")
 	}
 
-	fun setConsume(consume: OnConsume = DefaultConsume()) {
-		RabbitmqManager.setBasicConsume(channel!!, serverQueueName, consume)
+	fun setConsume(consume: DefaultConsume = DefaultConsume()) {
+		RabbitmqManager.setBasicConsume(channel!!, botQueueName, consume)
 	}
 
 	fun closeConnection() {
