f831d907cec06b5ea1ed861edf339574f30506b0
LovroG05 <lovro.govekar@gmail.com>
2022-02-22 15:16:56 +0100

    ooooh yeah

--- src/main/kotlin/org/classcompanion/botlib/objects/Entry.kt
+++ src/main/kotlin/org/classcompanion/botlib/objects/Entry.kt
@@ -5,8 +5,8 @@ import com.fasterxml.jackson.annotation.JsonProperty
 data class Entry(
 	@JsonProperty("id")
 	val id: Int,
-	@JsonProperty("userUuid")
-	val userUuid: String,
-	@JsonProperty("chatId")
-	val chatId: String
+	@JsonProperty("user_uuid")
+	val user_uuid: String,
+	@JsonProperty("chat_id")
+	val chat_id: String?
 )
